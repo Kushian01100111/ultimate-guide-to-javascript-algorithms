492462abc237c40b9cbc746797b0787a1aa31739
Kushian01100111 <comaspedro6@gmail.com>
2022-10-26 20:14:56 -0300

    Treee

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -6,11 +6,37 @@ return the equivalent of the sentence when capitalised. E.g
 
 
 
+//ForEach
+// function capSentence(text) {
+//     let wordsArray= text.toLowerCase().split(" "),
+//         capsArray = [];
 
+//     wordsArray.forEach(word =>{
+//       capsArray.push(word[0].toUpperCase() + word.slice(1))
+//     })
+
+//     return capsArray.join(" ")
+// }
+
+//map and slice
+// function capSentence(text) {
+//   let wordsArray = text.toLowerCase().split(" "),
+//       capsArray  = wordsArray.map(word =>{
+//         return word[0].toUpperCase() + word.slice(1)
+//       })
+//   return capsArray.join(" ")
+// }
+
+//map and replace
 function capSentence(text) {
-   // Code goes here
-}
+   let wordsArray = text.toLowerCase().split(" ")
 
+   let capsArray =  wordsArray.map(word => {
+    return word.replace(word[0], word[0].toUpperCase())
+   })
+
+   return capsArray.join(" ")
+}
 
 
 module.exports = capSentence
--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -5,8 +5,38 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 
 
+// function maxRecurringChar(text) {
+//     let charMap={},
+//         maxCharValue = 0,
+//         maxChar = '';
+//     for(let char of text){
+//         if(charMap.hasOwnProperty(char))charMap[char]++
+//         else charMap[char] = 1;
+//     }
+//     for(let char in charMap){
+//         if(charMap[char] > maxCharValue){
+//             maxCharValue= charMap[char]
+//             maxChar = char
+//         }
+//     }
+//     return maxChar
+// }
+
 function maxRecurringChar(text) {
-    // Code goes here
+    let charMap={},
+        charArray =[],
+        valueArray = [],
+        maxCharValue =0;
+    for(let char of text){
+        if(charMap.hasOwnProperty(char))charMap[char]++
+        else charMap[char] = 1;
+    }
+
+    charArray  = Object.keys(charMap)
+    valueArray = Object.values(charMap)
+    maxCharValue =  Math.max(...valueArray)
+
+    return charArray[valueArray.indexOf(maxCharValue)]
 }
 
 
